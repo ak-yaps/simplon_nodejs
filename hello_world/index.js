129ca221a639515139404d372a86281c0ec4db7a
yaps <yaps@yopmail.com>
2018-10-24 12:08:24 +0200

    first commit

---
+++ hello_world/index.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+var x = 0;
+
+while (x < 1000) {
+  console.log(x);
+  x += 100;
+}
+
+console.log(typeof document);
+console.log(typeof window);
+console.log(typeof '4');
+console.log(typeof NaN);
